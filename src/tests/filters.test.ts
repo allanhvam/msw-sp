e171fbde55e8850b37ac13e542fc6080c64b89d7
Allan Hvam <allanhp@hotmail.com>
2024-06-27 14:55:39 +0200

    feat: initial odata order by support

--- src/tests/filters.test.ts
+++ src/tests/filters.test.ts
@@ -9,25 +9,25 @@ import * as assert from "node:assert";
 import { describe, test } from "node:test";
 import { handlers } from '../handlers.js';
 
-void describe("lists", async () => {
+void describe("filters", async () => {
     const url = "https://tenant.sharepoint.com";
     const server = setupServer(...handlers({
         title: "tenant",
         url,
         sites: {
-            "empty": {
+            "filter": {
                 rootWeb: {
-                    title: "Empty Site",
-                    serverRelativeUrl: "/sites/empty",
+                    title: "Filter Site",
+                    serverRelativeUrl: "/sites/filter",
                     lists: [
                         {
-                            title: "Empty",
+                            title: "Filters",
                             id: "e3df5114-acc5-4901-8eb8-c55200d963d3",
                             baseTemplate: 100,
-                            url: "Lists/Empty",
+                            url: "lists/filters",
                             items: [
                                 {
-                                    Title: "Test"
+                                    Title: "Filter 1"
                                 }
                             ],
                             created: "2023-03-21T11:21:08Z",
@@ -54,15 +54,15 @@ void describe("lists", async () => {
     };
 
     await test("eq", async () => {
-        const sp = spfi().using(SPFx(getContext("/sites/empty")));
+        const sp = spfi().using(SPFx(getContext("/sites/filter")));
 
-        const lists = await sp.web.lists.filter("Title eq 'Empty'")();
+        const lists = await sp.web.lists.filter("Title eq 'Filters'")();
 
         assert.equal(lists.length, 1);
     });
 
     await test("gt", async () => {
-        const sp = spfi().using(SPFx(getContext("/sites/empty")));
+        const sp = spfi().using(SPFx(getContext("/sites/filter")));
 
         const lists = await sp.web.lists.filter("ItemCount gt 0")();
 
@@ -70,7 +70,7 @@ void describe("lists", async () => {
     });
 
     await test("ge", async () => {
-        const sp = spfi().using(SPFx(getContext("/sites/empty")));
+        const sp = spfi().using(SPFx(getContext("/sites/filter")));
 
         const lists = await sp.web.lists.filter("ItemCount ge 1")();
 
@@ -78,7 +78,7 @@ void describe("lists", async () => {
     });
 
     await test("lt datetime", async () => {
-        const sp = spfi().using(SPFx(getContext("/sites/empty")));
+        const sp = spfi().using(SPFx(getContext("/sites/filter")));
 
         const lists = await sp.web.lists.filter(`Created lt datetime'${new Date().toISOString()}'`)();
 
@@ -86,7 +86,7 @@ void describe("lists", async () => {
     });
 
     await test("gt datetime", async () => {
-        const sp = spfi().using(SPFx(getContext("/sites/empty")));
+        const sp = spfi().using(SPFx(getContext("/sites/filter")));
 
         const lists = await sp.web.lists.filter(`Created gt datetime'2020-01-01T12:00:00Z'`)();
 
